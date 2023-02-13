1cd9495faf0c463e82cae72fab98c288b8087a14
Anil kumar ravat <anilphp6@gmail.com>
2023-02-13 14:25:33 -0800

    Add files via upload

---
+++ hostApp/webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {ModuleFederationPlugin} = require("webpack").container;
+const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
+const path = require("path");
+const app1URL = 'http://localhost:3001';
+const app2URL = 'http://localhost:3002';
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    port: 3003,
+  },
+  output: {
+    publicPath: "auto",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "hostApp",
+      remotes: {
+        app1: "app1@[app1URL]/remoteEntry.js",
+		app2: "app2@[app2URL]/remoteEntry.js",
+      },
+      shared: {react: {singleton: true}, "react-dom": {singleton: true}},
+    }),
+    new ExternalTemplateRemotesPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
